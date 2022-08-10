e78316e68df49bb2d33e4ba9a4ced0547343bc78
Dela <stevedelatorre@gmail.com>
2022-08-09 21:14:50 -0400

    Hero Ability Details Part 2
    VideoPlayer support added to screen
    text layout changed
    panel layout changed

--- Assets/Sensei/Script/PanelNavigation.cs
+++ Assets/Sensei/Script/PanelNavigation.cs
@@ -23,7 +23,7 @@ public class PanelNavigation : MonoBehaviour, IDragHandler, IEndDragHandler
     private PointerEventData _lastPointerData;
 
     public Vector3 panelLocation;
-    public float percentThreshold = 0.35f;
+    public float percentThreshold = 0.45f;
 
     Vector3 panelSize = new Vector3(2340f, 1080f, 0f);
 
