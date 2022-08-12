ff401f42d56c5d7bd8fb37fcceb63c2670de7fd1
Dela <stevedelatorre@gmail.com>
2022-08-12 19:30:57 -0400

    Elaborating On AbilityData
    expanding the properties tracked for each hero ability

--- Assets/Sensei/Script/Data/AbilityData.cs
+++ Assets/Sensei/Script/Data/AbilityData.cs
@@ -22,16 +22,48 @@ public enum ControllerButton
     None
 }
 
+public enum WeaponQualities
+{
+    Melee,
+    Hitscan,
+    Projectile,
+    Explosive,
+    Beam,
+    Shotgun,
+    Linear,
+    Arcing,
+    SelfDamage,
+    Pinpoint,
+    Knockback,
+    Spread,
+    Healing,
+    Aura,
+    None
+}
+
 public class AbilityData
 {
+    public ControllerButton controllerButton = ControllerButton.Passive;
+
     public bool isUltimate = false;
+    public int ultimateCharge = 0;
 
     public string abilityName = "";
     public string abilityDetail = "";
     public string abilityVideo = "";
     public string abilityIcon = "";
 
-    public ControllerButton controllerButton = ControllerButton.Passive;
+    public int damage = 0;
+    public int healing = 0;
+    public int burst = 1;
+    public int pellets = 1;
+    public int ammo = 1;
+    public int range = 10;
+
+    public float reloadTime = 0.01f;
+    public float recoverTime = 0.01f;
+
+    public WeaponQualities[] weaponQualities = new WeaponQualities[] { WeaponQualities.None };
 
     public AbilityData()
     {
