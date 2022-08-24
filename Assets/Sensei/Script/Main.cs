5c74d1440e18794f1f20cf9ba2f51cc27b4b9182
Dela <stevedelatorre@gmail.com>
2022-08-24 15:49:30 -0400

    Full Ability Data Rework Part 2
    Ana through Genji had their data entered prior to this.
    This commit adds the detailed ability data for Hanzo through Orisa.

--- Assets/Sensei/Script/Main.cs
+++ Assets/Sensei/Script/Main.cs
@@ -33,41 +33,41 @@ public class Main : MonoBehaviour
 
     void InitializeAllHeroData()
     {
-        heroes[0] = new AnaHD();
-        heroes[1] = new AsheHD();
-        heroes[2] = new BaptisteHD();
-        heroes[3] = new BastionHD();
-        heroes[4] = new BrigitteHD();
-        heroes[5] = new CassidyHD();
-        heroes[6] = new DVaHD();
-        heroes[7] = new DoomfistHD();
-        heroes[8] = new EchoHD();
-        heroes[9] = new GenjiHD();
+        heroes[(int)HERO_ID.Ana] = new AnaHD();
+        heroes[(int)HERO_ID.Ashe] = new AsheHD();
+        heroes[(int)HERO_ID.Baptiste] = new BaptisteHD();
+        heroes[(int)HERO_ID.Bastion] = new BastionHD();
+        heroes[(int)HERO_ID.Brigitte] = new BrigitteHD();
+        heroes[(int)HERO_ID.Cassidy] = new CassidyHD();
+        heroes[(int)HERO_ID.Doomfist] = new DVaHD();
+        heroes[(int)HERO_ID.DVa] = new DoomfistHD();
+        heroes[(int)HERO_ID.Echo] = new EchoHD();
+        heroes[(int)HERO_ID.Genji] = new GenjiHD();
+        heroes[(int)HERO_ID.Hanzo] = new HanzoHD();
+        heroes[(int)HERO_ID.Junkrat] = new JunkratHD();
+        heroes[(int)HERO_ID.Lucio] = new LucioHD();
+        heroes[(int)HERO_ID.Mei] = new MeiHD();
+        heroes[(int)HERO_ID.Mercy] = new MercyHD();
+        heroes[(int)HERO_ID.Moira] = new MoiraHD();
+        heroes[(int)HERO_ID.Orisa] = new OrisaHD();
+        heroes[(int)HERO_ID.Pharah] = new PharahHD();
         /*
-        heroes[10] = new HanzoHD();
-        heroes[11] = new JunkratHD();
-        heroes[12] = new LucioHD();
-        heroes[13] = new MeiHD();
-        heroes[14] = new MercyHD();
-        heroes[15] = new MoiraHD();
-        heroes[16] = new OrisaHD();
-        heroes[17] = new PharahHD();
-        heroes[18] = new ReaperHD();
-        heroes[19] = new ReinhardtHD();
-        heroes[20] = new RoadhogHD();
-        heroes[21] = new SigmaHD();
-        heroes[22] = new Soldier76HD();
-        heroes[23] = new SombraHD();
+        heroes[(int)18] = new ReaperHD();
+        heroes[(int)19] = new ReinhardtHD();
+        heroes[(int)20] = new RoadhogHD();
+        heroes[(int)21] = new SigmaHD();
+        heroes[(int)22] = new Soldier76HD();
+        heroes[(int)23] = new SombraHD();
         */
-        heroes[24] = new SymmetraHD();
+        heroes[(int)24] = new SymmetraHD();
         /*
-        heroes[25] = new TorbjornHD();
-        heroes[26] = new TracerHD();
-        heroes[27] = new WidowmakerHD();
-        heroes[28] = new WinstonHD();
-        heroes[29] = new WreckingBallHD();
-        heroes[30] = new ZaryaHD();
-        heroes[31] = new ZenyattaHD();
+        heroes[(int)25] = new TorbjornHD();
+        heroes[(int)26] = new TracerHD();
+        heroes[(int)27] = new WidowmakerHD();
+        heroes[(int)28] = new WinstonHD();
+        heroes[(int)29] = new WreckingBallHD();
+        heroes[(int)30] = new ZaryaHD();
+        heroes[(int)31] = new ZenyattaHD();
         */
     }
 }
